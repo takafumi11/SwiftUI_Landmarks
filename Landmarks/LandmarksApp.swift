cdcb3070cc601440ab51e9c8a01b32e405b9eb52
Noiri, Takafumi | Taka | ELPD <takafumi.noiri@rakuten.com>
2022-04-17 20:53:36 +0900

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Noiri, Takafumi | Taka | ELPD on 2022/04/17.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
