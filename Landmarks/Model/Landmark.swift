aac27a31787eeeaab8f994bf9f2cfa59547e9710
noirit7@gmail.com <takafumi.noiri@rakuten.com>
2022-04-20 23:23:07 +0900

    Building Lists and Navigation section 7 まで

--- Landmarks/Model/Landmark.swift
+++ Landmarks/Model/Landmark.swift
@@ -9,26 +9,27 @@ import Foundation
 import SwiftUI
 import CoreLocation
 
-struct Landmark: Hashable, Codable {
+struct Landmark: Hashable, Codable, Identifiable {
     var id: Int
     var name: String
     var park: String
     var state: String
-    var descrption: String
-    
+    var description: String
+
     private var imageName: String
     var image: Image {
         Image(imageName)
     }
-    
+
     private var coordinates: Coordinates
     var locationCoordinate: CLLocationCoordinate2D {
-        CLLocationCoordinate2D(latitude: coordinates.latitude, longitude: coordinates.longitude)
+        CLLocationCoordinate2D(
+            latitude: coordinates.latitude,
+            longitude: coordinates.longitude)
     }
-    
+
     struct Coordinates: Hashable, Codable {
         var latitude: Double
         var longitude: Double
     }
-    
 }
---
+++ Landmarks/Views/LandmarkList.swift
@@ -0,0 +1,30 @@
+//
+//  LandmarkList.swift
+//  Landmarks
+//
+//  Created by Noiri, Takafumi | Taka | ELPD on 2022/04/19.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    var body: some View {
+        NavigationView {
+            List(landmarks) { landmark in
+                NavigationLink {
+                    LandmarkDetail(landmark: landmark)
+                } label: {
+                    LandmarkRow(landmark: landmark)
+                }
+                
+            }
+            .navigationTitle("Landmarks")
+        }
+    }
+}
+
+struct LandmarkList_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkList()
+    }
+}
