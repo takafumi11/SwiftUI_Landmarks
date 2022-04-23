ee3cab8841d95f86e2268d4d08e22d734a6e7ca5
noirit7@gmail.com <takafumi.noiri@rakuten.com>
2022-04-23 20:33:43 +0900

    Handling User Input 終わり？

--- Landmarks/Views/Landmarks/LandmarkRow.swift
+++ Landmarks/Views/Landmarks/LandmarkRow.swift
@@ -21,11 +21,19 @@ struct LandmarkRow: View {
             
             Spacer()
             
+            if landmark.isFavorite {
+                Image(systemName: "star.fill")
+                    .foregroundColor(.yellow)
+                
+            }
+            
         }
     }
 }
 
 struct LandmarkRow_Previews: PreviewProvider {
+    static var landmarks = ModelData().landmarks
+    
     static var previews: some View {
         Group {
             LandmarkRow(landmark: landmarks[0])
